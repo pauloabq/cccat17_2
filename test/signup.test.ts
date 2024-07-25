b9d0803990ed4fe906282e7769493e5a00ce9613
Paulo Albuquerque <pcajunior@gmail.com>
2024-07-24 22:46:06 -0300

    adiciona teste de nome com erro

--- test/signup.test.ts
+++ test/signup.test.ts
@@ -14,3 +14,14 @@ test("Deve criar uma conta para o passageiro", async function () {
   expect(outputGetAccount.email).toBe(input.email);
   expect(outputGetAccount.cpf).toBe(input.cpf);
 });
+
+test("Não deve criar uma conta para o passageiro com nome inválido", async function () {
+  const inputNameError = {
+    name: "",
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: "28722437045",
+    isPassenger: true
+  }
+  const outputSignUpNameError = await signup(inputNameError);
+  expect(outputSignUpNameError).toBe(-3);
+});
