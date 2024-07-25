e1a4ac2dbd06ca951db69616f7ae6ec46c8701a4
Paulo Albuquerque <pcajunior@gmail.com>
2024-07-24 23:27:48 -0300

    refatora

--- src/signup.ts
+++ src/signup.ts
@@ -1,63 +1,24 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
 import { validateCpf } from "./validateCpf";
-
 export async function signup (input: any): Promise<any> {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
 	try {
-		const id = crypto.randomUUID();
-
-		const [acc] = await connection.query("select * from cccat17.account where email = $1", [input.email]);
-		if (!acc) {
-
-			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (input.email.match(/^(.+)@(.+)$/)) {
-
-					if (validateCpf(input.cpf)) {
-						if (input.isDriver) {
-							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-								
-								const obj = {
-									accountId: id
-								};
-								return obj;
-							} else {
-								// invalid car plate
-								return -5;
-							}
-						} else {
-							await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-							const obj = {
-								accountId: id
-							};
-							return obj;
-						}
-					} else {
-						// invalid cpf
-						return -1;
-					}
-				} else {
-					// invalid email
-					return -2;
-				}
-
-			} else {
-				// invalid name
-				return -3;
-			}
-
-		} else {
-			// already exists
-			return -4;
-		}
-
+		const accountId = crypto.randomUUID();
+		const [existingAccount] = await connection.query("select * from cccat17.account where email = $1", [input.email]);
+		if (existingAccount) return -4;
+    if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) return -3;
+    if (!input.email.match(/^(.+)@(.+)$/)) return -2;
+    if (!validateCpf(input.cpf)) return -1;
+    if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) return -5;
+    await connection.query("insert into cccat17.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
+    return {
+      accountId
+    };
 	} finally {
-		await connection.$pool.end();
+		await connection.$pool.end(); 
 	}
 }
-
 export async function getAccount (accountId: any): Promise<any> {
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
   const [account] = await connection.query("select * from cccat17.account where account_id = $1", [accountId]);
